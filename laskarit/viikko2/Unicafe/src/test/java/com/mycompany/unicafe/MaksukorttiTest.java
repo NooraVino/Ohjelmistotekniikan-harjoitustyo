1f092ff9049dc6a1e829a356247ba5cc1b1effba
Noora <noora.virolainen@yahoo.com>
2018-11-13 21:18:09 +0200

    palautus

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -12,30 +12,34 @@ public class MaksukorttiTest {
     public void setUp() {
         kortti = new Maksukortti(10);
     }
-    @Test 
-        public void alkuSaldoOikein() {
+
+    @Test
+    public void alkuSaldoOikein() {
         String vastaus = kortti.toString();
         assertEquals("saldo: 0.10", vastaus);
     }
-        
+
     @Test
     public void RahanLatausKasvattaaSaldoaOikein() {
         kortti.lataaRahaa(25);
         assertEquals("saldo: 0.35", kortti.toString());
-    } 
-    
+    }
+
     @Test
     public void otaRahaaVahentaaSaldoaOikein() {
         kortti.lataaRahaa(25);
         kortti.otaRahaa(10);
         assertEquals("saldo: 0.25", kortti.toString());
     }
-    
 
-        
+    @Test
+    public void otaRahaaEiVahennaNegatiiviseksi() {
+        kortti.otaRahaa(15);
+        assertEquals("saldo: 10.00", kortti.toString());
+    }
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
     }
 }
