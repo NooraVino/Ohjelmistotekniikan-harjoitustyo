1892025fb099262225fc8e9c6a492a595b2b42f6
Noora Virolainen <noora.virolainen@yahoo.com>
2018-11-15 11:40:58 +0200

    Sovelluksen luomista

--- GiftWish/src/main/java/nooran/giftwish/Paaohjelma.java
+++ GiftWish/src/main/java/nooran/giftwish/Paaohjelma.java
@@ -3,18 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package nooran.giftwish;
 
 /**
  *
  * @author vino
  */
-public class PaaOhjelma {
+public class Paaohjelma {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("nsci");
+        System.out.println("nfifj");
     }
     
 }
