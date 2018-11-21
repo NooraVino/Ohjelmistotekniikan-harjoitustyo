e3c4caa1ad353d3d80e629a86ac148e0a2b87939
Noora <noora.virolainen@yahoo.com>
2018-11-21 13:38:40 +0200

    käyttöliittymäbn aloitus

---
+++ GiftWish/src/main/java/nooran/giftwish/dao/FileUserDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nooran.giftwish.dao;
+
+/**
+ *
+ * @author vino
+ */
+public class FileUserDao  {
+    
+    public FileUserDao(String file) {
+    
+}
+}
--- GiftWish/src/main/java/nooran/giftwish/ui/giftWishUi.java
+++ GiftWish/src/main/java/nooran/giftwish/ui/giftWishUi.java
@@ -5,6 +5,8 @@
  */
 package nooran.giftwish.ui;
 
+import java.io.FileInputStream;
+import java.util.Properties;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,6 +14,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import nooran.giftwish.dao.FileGiftDao;
+import nooran.giftwish.dao.FileUserDao;
 
 /**
  *
@@ -19,7 +23,19 @@ import javafx.stage.Stage;
  */
 public class giftWishUi extends Application {
     
+@Override
+    public void init() throws Exception {
+        Properties properties = new Properties();
 
+        properties.load(new FileInputStream("config.properties"));
+        
+        String userFile = properties.getProperty("userFile");
+        String giftFile = properties.getProperty("giftFile");
+            
+        FileUserDao userDao = new FileUserDao(userFile);
+        FileGiftDao giftDao = new FileGiftDao();
+        //todoService = new TodoService(todoDao, userDao);
+    }
 
   @Override
     public void start(Stage ikkuna) {
